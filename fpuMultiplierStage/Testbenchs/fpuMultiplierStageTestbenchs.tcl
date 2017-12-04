3bfa81b124f660c2558235d0ae3ca047e7d3dd9c
priyankasistla <34215632+priyankasistla@users.noreply.github.com>
2017-12-04 15:31:43 -0500

    Added the final test benches too

--- fpuMultiplierStage/Testbenchs/fpuMultiplierStageTestbenchs.tcl
+++ fpuMultiplierStage/Testbenchs/fpuMultiplierStageTestbenchs.tcl
@@ -1,2 +1,4 @@
 vcom -reportprogress 300 -work work ./fpuMultiplierStage/Testbenchs/cmaMultiplier_tb.vhdl
 vcom -reportprogress 300 -work work ./fpuMultiplierStage/Testbenchs/boothMultiplier_tb.vhdl
+vcom -reportprogress 300 -work work ./fpuMultiplierStage/Testbenchs/multiplierStage_tb.vhdl
+vcom -reportprogress 300 -work work ./fpuMultiplierStage/Testbenchs/multiplierStage_test_vect.vhdl
